8f3eeeff807251e602908f2ef268e56bbab71875
justinsamuel22 <justinsamuel22@icloud.com>
2025-03-29 22:16:59 +0000

    Removed empty TSX fragments

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="tracking-tighter font-extrabold">Justin Samuel</h1>
-        <h2 className="tracking-tight font-medium">Vulnerability Analyst @ Virgin Money</h2>
-        <h2 className="tracking-tight font-medium">Aspiring DevSecOps Engineer</h2>
-        <h2 className="tracking-tight font-medium">Based in Newcastle, UK</h2>
-      </div>
-    </>
+    <div>
+      <h1 className="tracking-tighter font-extrabold">Justin Samuel</h1>
+      <h2 className="tracking-tight font-medium">Vulnerability Analyst @ Virgin Money</h2>
+      <h2 className="tracking-tight font-medium">Aspiring DevSecOps Engineer</h2>
+      <h2 className="tracking-tight font-medium">Based in Newcastle, UK</h2>
+    </div>
   );
 }
 
