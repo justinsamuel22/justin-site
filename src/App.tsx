78440e662427c93f983015eb449ea31ac6fa6684
justinsamuel22 <justinsamuel22@icloud.com>
2025-03-29 19:37:04 +0000

    Inital commit with basic layout

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="tracking-tighter font-extrabold">Justin Samuel</h1>
+        <h2 className="tracking-tight font-medium">
+          Vulnerability Analyst @ Virgin Money
+        </h2>
+        <h2 className="tracking-tight font-medium">
+          Aspiring DevSecOps Engineer
+        </h2>
+        <h2 className="tracking-tight font-medium">Based in Newcastle, UK</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
